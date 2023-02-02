818c0e220f922e13e20dd749d80c004424b2f62a
Zd <1207366899@qq.com>
2023-02-02 19:59:25 +0800

    feat: SR-W2-DToFo0 原定制3D地图支持点位聚合及弹窗展示详情 (#503)
    Co-authored-by: zhangdian <zhangdian@njsdata.com>

--- 武汉项目zfw/1.4-SR-W53-v4keih-二开cesium地图支持加载3DTile地图服务-analyzer-vue-plugin/src/components/development/mockData.js
+++ 武汉项目zfw/1.4-SR-W53-v4keih-二开cesium地图支持加载3DTile地图服务-analyzer-vue-plugin/src/components/development/mockData.js
@@ -200,6 +200,47 @@ export const mockCustomConfig = {
         },
       ],
     },
+    {
+      modalWidth: 1400,
+      modalHeight: 700,
+      LongitudeKey: "经度",
+      LatitudeKey: "纬度",
+      modalTitle: "A",
+      modalTableColumn: [
+        {
+          width: 100,
+          column: "城市",
+        },
+        {
+          width: 100,
+          column: "种类",
+        },
+        {
+          width: 100,
+          column: "利润",
+        },
+        {
+          width: 100,
+          column: "销售额",
+        },
+      ],
+      showCondition: [
+        {
+          value: "南京市",
+          column: "城市",
+        },
+      ],
+      name: "点位详情2点位详情2点位详情2点位详情2",
+      backgroundList: [],
+      imgList: [
+        {
+          uid: 1673251946982,
+          name: "custom_table.png",
+          url: "/storage_area/form/1234567890/7f81d4c20ff6420a911cdb1e4609862d.png",
+          status: "success",
+        },
+      ],
+    },
   ],
   ServerImageryProvider:
     "https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer",
