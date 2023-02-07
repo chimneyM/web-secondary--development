c3dd8f50259616838d6893c30806cebd59c4af83
拾肆 <71803979+1977953215@users.noreply.github.com>
2023-02-07 14:11:15 +0800

    fix：增加已登录判断 (#512)

--- 2022.11.1-SR-W44-lNKI34-安吉交投-SSO-app-sso-react-plugin/src/App.js
+++ 2022.11.1-SR-W44-lNKI34-安吉交投-SSO-app-sso-react-plugin/src/App.js
@@ -4,94 +4,126 @@ import { queryCurrentUser, ssoLoginNew, isSso, tokenIsValid, setCookie } from ".
 import Cookies from "js-cookie";
 
 const App = (props) => {
-  const { loginStatus = () => {}, updateProcess } = props;
+   const { loginStatus = () => {}, updateProcess } = props;
 
-  useEffect(() => {
-    updateProcess && updateProcess();
-    newSsoFuc();
-  }, []); // eslint-disable-next-line react-hooks/exhaustive-deps
+   useEffect(() => {
+      updateProcess && updateProcess();
+      newSsoFuc();
+   }, []); // eslint-disable-next-line react-hooks/exhaustive-deps
 
-  const newSsoFuc = async () => {
-    try {
-      const { sso_new_flag } = window.configuration || {};
-      const { data } = await queryCurrentUser();
+   const newSsoFuc = async () => {
+      console.log("<--进入二开SSO组件-->");
 
-      if (!data && sso_new_flag?.current_value === "1") {
-        const ssoType = getQueryString("ssoType");
-        const { data: ssoResult } = await isSso(ssoType);
-        // 获取sso配置，存在配置则走sso跳转逻辑，否则走正常跳转逻辑
-        if (ssoResult) {
-          const { tokenName, ssoType: _ssoType } = ssoResult;
-          if (_ssoType === "application") {
-            let token = "";
-            // 判断从url或cookies中取token
-            if (ssoResult?.tokenFlag === 1) {
-              token = getQueryString(tokenName);
-            } else if (ssoResult?.tokenFlag === 2) {
-              token = getQueryStringCookies(tokenName);
+      try {
+         const { sso_new_flag } = window.configuration || {};
+         const { data } = await queryCurrentUser();
+
+         console.log("用户数据-------->", data);
+
+         if (!data && sso_new_flag?.current_value === "1") {
+            const ssoType = getQueryString("ssoType");
+            const { data: ssoResult } = await isSso(ssoType);
+
+            console.log("未登录", ssoResult);
+
+            // 获取sso配置，存在配置则走sso跳转逻辑，否则走正常跳转逻辑
+            if (ssoResult) {
+               const { tokenName, ssoType: _ssoType } = ssoResult;
+               if (_ssoType === "application") {
+                  console.log("<--ssoType验证通过-->");
+                  let token = "";
+                  // 判断从url或cookies中取token
+                  if (ssoResult?.tokenFlag === 1) {
+                     token = getQueryString(tokenName);
+                     console.log("<--从Url中获取Token-->", token);
+                  } else if (ssoResult?.tokenFlag === 2) {
+                     token = getQueryStringCookies(tokenName);
+                  }
+
+                  // 如果没有token直接跳转配置的地址
+                  if (!token) {
+                     failLogin(ssoResult);
+                     return;
+                  }
+
+                  // 校验token是否有效
+                  tokenIsValid(token)
+                     .then((res) => {
+                        setCookie(token);
+                        window.SSO_IS_LOGIN = true;
+                        loginStatus({
+                           flag: true,
+                           ssoResult,
+                        });
+                     })
+                     .catch((err) => {
+                        failLogin(ssoResult);
+                     });
+               } else {
+                  failLogin(ssoResult);
+               }
+            } else {
+               failLogin();
             }
-            // 如果没有token直接跳转配置的地址
-            if (!token) {
-              failLogin(ssoResult);
-              return;
+         } else {
+            const ssoType = getQueryString("ssoType");
+            const { data: ssoResult } = await isSso(ssoType);
+
+            if (ssoResult) {
+               const { tokenName, ssoType: _ssoType } = ssoResult;
+
+               if (_ssoType === "application") {
+                  let token = "";
+                  if (ssoResult?.tokenFlag === 1) {
+                     token = getQueryString(tokenName);
+                  } else if (ssoResult?.tokenFlag === 2) {
+                     token = getQueryStringCookies(tokenName);
+                  }
+
+                  if (token) {
+                     tokenIsValid(token).then((res) => {
+                        setCookie(token);
+                        window.SSO_IS_LOGIN = true;
+                        loginStatus({
+                           flag: true,
+                           ssoResult,
+                        });
+                     });
+                  }
+               }
             }
-            // 校验token是否有效
-            tokenIsValid(token)
-              .then((res) => {
-                setCookie(token);
-                window.SSO_IS_LOGIN = true;
-                loginStatus({
-                  flag: true,
-                  ssoResult,
-                });
-              })
-              .catch((err) => {
-                failLogin(ssoResult);
-              });
-          } else {
-            failLogin(ssoResult);
-          }
-        } else {
-          failLogin();
-        }
+         }
+      } catch (error) {
+         console.log(error);
+         failLogin();
       }
-    } catch (error) {
-      console.log(error);
-      failLogin();
-    }
-  };
+   };
 
-  const failLogin = async (ssoResult) => {
-    console.log("SSO登录失败!");
-    if (ssoResult) {
-      loginStatus({
-        flag: false,
-        ssoResult,
-      });
-    } else {
-      let configuration = sessionStorage.getItem("sessionValue");
-      configuration = JSON.parse(configuration);
+   const failLogin = async (ssoResult) => {
+      console.log("SSO登录失败!");
 
-      const { sso_fail_url = {} } = configuration;
-      const { current_value } = sso_fail_url;
-
-      if (current_value) {
-        window.location.href = current_value;
+      if (ssoResult) {
+         loginStatus({
+            flag: false,
+            ssoResult,
+         });
+      } else {
+         window.location.href = "https://jt-toolkit.ajjtcx.com/";
+         // window.location.href = "https://jtdc-ls-toolkit.ajjtcx.com";
       }
-    }
-  };
+   };
 
-  return <></>;
+   return <></>;
 };
 export default App;
 
 const getQueryString = (name) => {
-  const reg = new RegExp(name + "=([^&]*)(&|$)", "i");
-  const r = window.location.search.substr(1).match(reg);
-  if (r != null) return r[1];
-  return "";
+   const reg = new RegExp(name + "=([^&]*)(&|$)", "i");
+   const r = window.location.search.substr(1).match(reg);
+   if (r != null) return r[1];
+   return "";
 };
 
 const getQueryStringCookies = (name) => {
-  return Cookies.get(name);
+   return Cookies.get(name);
 };
